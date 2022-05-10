33e035bcc8e39208740aeb24d446f4386c325947
patrick snowden <patrickbsnowden@outlook.com>
2022-05-10 11:32:39 -0700

    Changed welcome message and added OOP!

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
 
-function getFetch(){
+function getFetch() {
   input = document.querySelector("input").value.split(" ").join("_");
   const url = `https://api.pokemontcg.io/v2/cards?q=name:${input}`;
   console.log(url);
@@ -8,19 +8,15 @@ function getFetch(){
   fetch(url)
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
-
-      console.log(data);
-      clear();
-      showCards();
-      hideWelcome();
-      
+      const card = new TradingCard(data.data);
+      card.clear();
+      card.cardInfo();
+      card.showCards();
+      card.hideWelcome();
       if (data.data.length > 0) {
-        data.data.forEach((card) => {
-          console.log(card);
-          getImage(card);
-        });
+        card.getInfo();
       } else {
-        notAPokemon()
+        card.notAPokemon();
       }
     })
 
@@ -28,72 +24,77 @@ function getFetch(){
       console.log(`error ${err}`);
     });
 }
-   
-function clear() {
-  const cardContainer = document.querySelector(".cardContainer");
-  while (cardContainer.firstChild) {
-    cardContainer.removeChild(cardContainer.firstChild);
+
+class TradingCard {
+  constructor(cards) {
+    this.cards = cards;
   }
-}
 
-function getImage(card) {
-  if (card.tcgplayer) {
-    let prices = card.tcgplayer.prices;
-    let link = card.tcgplayer.url;
-    let priceRanges = Object.values(prices)[0];
-
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <div>
-            <div class="pb-2">
-                <a href="${link}" target="_blank"><img src="${card.images.small}"></img></a>
-            </div>
-            <div class="prices">
-                <h2 class="bg-red-100 p-1.5 text-lg rounded-t-md">Low - $${priceRanges.low}</h2>
-                <h2 class="bg-blue-100 p-1.5 text-lg">High - $${priceRanges.high}</h2>
-                <h2 class="bg-green-100 p-1.5 text-lg">Market - $${priceRanges.market}</h2>
-                <h2 class="bg-gray-200 p-1.5 text-lg rounded-b-md font-bold"><a href="${link}" target="_blank"">Check TCG player</a></h2>
-            </div>
-
-        </div>
-        `;
-    document.querySelector(".cardContainer").appendChild(div);
-  } else {
-    const div = document.createElement("div");
-    div.innerHTML = `
-        <div>
-            <div class="pb-2">
-                <a href="#" target="_blank"><img src="${card.images.small}" width="245" height="342"></img></a>
-            </div>
-            <div class="prices">
-                <h2 class="bg-red-100 p-1.5 text-lg rounded-t-md">n/a</h2>
-                <h2 class="bg-blue-100 p-1.5 text-lg">n/a</h2>
-                <h2 class="bg-green-100 p-1.5 text-lg">n/a</h2>
-                <h2 class="bg-gray-200 p-1.5 text-lg rounded-b-md font-bold">n/a</h2>
-            </div>
-
-        </div>
-        `;
-    document.querySelector(".cardContainer").appendChild(div);
+  clear() {
+    const cardContainer = document.querySelector(".cardContainer");
+    while (cardContainer.firstChild) {
+      cardContainer.removeChild(cardContainer.firstChild);
+    }
   }
-}
 
-function showCards() {
-  document.querySelector(".cardContainer").classList.remove("hidden");
-  document.querySelector(".cardContainer").classList.add("flex");
-}
+  cardInfo() {
+    for (const card of this.cards) {
+      if (card.tcgplayer) {
+        let price = Object.values(card.tcgplayer.prices)[0];
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div>
+                    <div class="pb-2">
+                        <a href="${card.tcgplayer.url}" target="_blank"><img src="${card.images.small}"></img></a>
+                    </div>
+                    <div class="prices">
+                        <h2 class="bg-red-100 p-1.5 text-lg rounded-t-md">Low - $${price.low}</h2>
+                        <h2 class="bg-blue-100 p-1.5 text-lg">High - $${price.high}</h2>
+                        <h2 class="bg-green-100 p-1.5 text-lg">Market - $${price.market}</h2>
+                        <h2 class="bg-gray-200 p-1.5 text-lg rounded-b-md font-bold"><a href="${card.tcgplayer.url}" target="_blank"">Check TCG player</a></h2>
+                    </div>
+        
+                </div>
+                `;
+        document.querySelector(".cardContainer").appendChild(div);
+      } else {
+        const div = document.createElement("div");
+        div.innerHTML = `
+                <div>
+                    <div class="pb-2">
+                        <a href="#" target="_blank"><img src="${card.images.small}" width="245" height="342"></img></a>
+                    </div>
+                    <div class="prices">
+                        <h2 class="bg-red-100 p-1.5 text-lg rounded-t-md">n/a</h2>
+                        <h2 class="bg-blue-100 p-1.5 text-lg">n/a</h2>
+                        <h2 class="bg-green-100 p-1.5 text-lg">n/a</h2>
+                        <h2 class="bg-gray-200 p-1.5 text-lg rounded-b-md font-bold">n/a</h2>
+                    </div>
+        
+                </div>
+                `;
+        document.querySelector(".cardContainer").appendChild(div);
+      }
+    }
+  }
 
-function hideWelcome() {
-  document.querySelector(".welcome").classList.add("hidden");
-}
+  showCards() {
+    document.querySelector(".cardContainer").classList.remove("hidden");
+    document.querySelector(".cardContainer").classList.add("flex");
+  }
 
-function notAPokemon() {
-  const input = document.querySelector("input")
-  input.classList.add("bg-red-200");
-  input.value = ''
-  input.placeholder = 'Wrong spelling'
-  setTimeout(() => {
-    input.classList.remove("bg-red-200");
-    input.placeholder = 'Enter Pokemon'
-  }, 3000);
+  hideWelcome() {
+    document.querySelector(".welcome").classList.add("hidden");
+  }
+
+  notAPokemon() {
+    const input = document.querySelector("input");
+    input.classList.add("bg-red-200");
+    input.value = "";
+    input.placeholder = "Wrong spelling";
+    setTimeout(() => {
+      input.classList.remove("bg-red-200");
+      input.placeholder = "Enter Pokemon";
+    }, 3000);
+  }
 }
